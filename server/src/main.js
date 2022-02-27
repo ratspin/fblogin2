af6977c280a58a3d68772e5358bee125b0b53231
ratspin <6210110013@psu.ac.th>
2022-02-28 04:32:21 +0700

    verify token

--- server/src/main.js
+++ server/src/main.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const axios = require('axios')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/api/login', bodyParser.json(), async (req,res) =>{
+    let token = req.body.token
+    let result = await axios.get('https://graph.facebook.com/me',{
+        params: {
+            field: 'id,name,email',
+            access_token: token
+        }
+    })
+    console.log(result.data)
+    res.send({ok: 1})
 })
 
 app.listen(port, () => {
