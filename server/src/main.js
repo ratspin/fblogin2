5594d98eaaec883728800379b3f849983399d943
ratspin <6210110013@psu.ac.th>
2022-02-28 13:01:47 +0700

    issue token

--- server/src/main.js
+++ server/src/main.js
@@ -4,6 +4,7 @@ const port = 8080
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const cors = require('cors')
+const TOKEN_SECRET = '92c71949d9b8579b2f70ef1f8c6b46506be88eeae25707f05c16a3c7983f1e9a3b1823b06ae11b06320bb1d7760938de9944c4318080f61f4d19decfe30dec96'
 
 app.use(cors())
 
@@ -15,14 +16,22 @@ app.post('/api/login', bodyParser.json(), async (req,res) =>{
     let token = req.body.token
     let result = await axios.get('https://graph.facebook.com/me',{
         params: {
-            field: 'id,name,email',
+            fields: 'id,name,email',
             access_token: token
         }
     })
-    console.log(result.data)
-    res.send({ok: 1})
+    if(!result.data.id){
+        res.sendStatus(403)
+        return
+    }
+        let data = {
+            username : result.data.email
+        }
+        let access_token = jwt.sign(data, TOKEN_SECRET, {expiresIn: '1800s'})
+        res.send({access_token, username: data.username})
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
